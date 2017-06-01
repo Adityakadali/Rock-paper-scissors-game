0f19f6d9919670761aaf228e2d41ce3be5c02262
Adityakadali <adityakadali00@gmail.com>
2017-06-01 19:43:08 +0530

    Update rock.py

--- rock.py
+++ rock.py
@@ -1,22 +1,24 @@
 import random
-comp = 0
-you = 0
-def scorecard(number):
+
+cont = 'y' # For defining while loop exiting
+
+
+
+def scorecard(number):   # who are getting a point
 	if number == 1 :
 		print('Draw')
 		
 	elif number == 2 :
 		print('Computer got a point')
-		comp = comp + 1
+		
+	
 		
 	elif number == 3:
 		print('You got a point')
-		you = you + 1
-	print('comp **** you')
-	print(comp+ '    ' + you)
 		
 
-def choice(n):
+
+def choice(n):   # assing numbers for rock paper scissor of computer choice
 	if n == 1 :
 		print('Rock')
 	elif n == 2 :
@@ -28,10 +30,12 @@ def choice(n):
 
 def user():
 	user_input = input('choose anything \n1.rock \n2.paper \n3.scissors\n \n')
-	game = (random.randint(1,3))
+	game = (random.randint(1,3)) #computer choice of rock,paper scissor
+	print('#'*50) # printint * 50 times for looking good in terminal
+	choice(game)  # printing computer choice
 	print('#'*50)
-	choice(game)
-	print('#'*50)
+	'''these if else loop defines which beats which 
+	for example paper beats rock '''
 	if user_input == '1':
 		if game == 1:
 			scorecard(1)
@@ -54,12 +58,18 @@ def user():
 		elif game ==3:
 			scorecard(1)
 	else:
-		print('Enter a valid choice')
+		print('Enter a valid choice') # if user doesn't makes a valid choice
 
-while 1 == 1:
 
-	user()
+
+while cont == 'y' or cont == 'Y':
+    
+	r = user() # storing user value in r 
+	print(r) # print r
+
 	print('*'*50 +'\n'+'*'*50)
-
+	cont = input (' Do you want to continue \'y\' for yes any key to exit :  ')
+	print (cont)  # ask user weather he wants to continue or not
+	print('*'* 50,'\n','*'*50 )
 
 
